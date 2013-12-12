8cd29407911ac12847dcd70464ef49663ca279f5
Jacek <jacek@ivolution.pl>
2013-12-12 23:47:09 +0100

    test

--- class/displayManager.Class.php
+++ class/displayManager.Class.php
@@ -131,7 +131,7 @@ class displayManager extends smarty{
 		$this->secure('self');
 		$meetings = $this->engine->loadMeetings();
 		foreach ($meetings as $key => $val){
-			if ($val->date != $data['date'] || $val->r_amount >= $val->persons_limit){
+			if ($val->date != $data['date']){
 				var_dump(11);
 				unset($meetings[$key]);
 			}
