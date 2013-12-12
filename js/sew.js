095f61750c8458b75ef7d9cb39efefbb9580180c
Jacek <jacek@ivolution.pl>
2013-12-12 23:32:43 +0100

    test

--- js/sew.js
+++ js/sew.js
@@ -8,7 +8,6 @@ var sew = {
 			$('#meeting').html('<p>Wczytuję dane</p>').load("/index.php",{'action':'m_date'}, 
 				function (){
 					$('#m_date').change(function(){
-						console.log($('#m_date').fieldValue())
 	    				$('#select_hour').html('Wczytuję dane').load('/index.php',{'action':'m_time','date':$('#m_date').fieldValue()})});
 	    	});
 		return false;
